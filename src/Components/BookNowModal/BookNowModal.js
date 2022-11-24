da26599dd819a9b6644a8b778cbdee315da5de66
noman <nomannabil30@gmail.com>
2022-11-25 00:03:21 +0600

    booking modal created

--- src/Components/BookNowModal/BookNowModal.js
+++ src/Components/BookNowModal/BookNowModal.js
@@ -20,6 +20,8 @@ const BookNowModal = (props) => {
 		const price = form.price.value;
 		const meetingPlace = form.meetingPlace.value;
 		console.log(fullname, email, phoneNumber, carModel, price, meetingPlace);
+
+		setBookNowModalData(null);
 	};
 
 	return (
@@ -47,7 +49,7 @@ const BookNowModal = (props) => {
 										>
 											<div className='grid gap-6 mb-6'>
 												<div>
-													<label className='block mb-2 text-sm font-medium text-gray-900 '>Full Name</label>
+													<label className='block mb-2 text-sm font-medium text-gray-900 '>User Name</label>
 													<input
 														type='text'
 														name='fullname'
@@ -68,15 +70,7 @@ const BookNowModal = (props) => {
 														required
 													/>
 												</div>
-												<div>
-													<label className='block mb-2 text-sm font-medium text-gray-900'>Phone number</label>
-													<input
-														name='phoneNumber'
-														className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 '
-														placeholder='123-45-678'
-														required
-													/>
-												</div>
+
 												<div>
 													<label className='block mb-2 text-sm font-medium text-gray-900 '>Car Model</label>
 													<input
@@ -102,6 +96,15 @@ const BookNowModal = (props) => {
 													/>
 												</div>
 												<div>
+													<label className='block mb-2 text-sm font-medium text-gray-900'>Phone number</label>
+													<input
+														name='phoneNumber'
+														className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 '
+														placeholder='123-45-678'
+														required
+													/>
+												</div>
+												<div>
 													<label className='block mb-2 text-sm font-medium text-gray-900 '>Meeting Place</label>
 													<input
 														name='meetingPlace'
@@ -112,10 +115,7 @@ const BookNowModal = (props) => {
 												</div>
 											</div>
 
-											<button
-												type='submit'
-												className='text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800'
-											>
+											<button type='submit' className='text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded text-sm w-full sm:w-auto px-5 py-2.5 text-center '>
 												Submit
 											</button>
 										</form>
